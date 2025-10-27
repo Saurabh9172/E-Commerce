13c9d61b2b2d5abb03f42a59d98cc697c72677f3
Saurabh9172 <162452149+Saurabh9172@users.noreply.github.com>
2025-10-27 14:10:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/images',express.static('upload/images'));
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`ecommerce-fwczaqa5cwdrfsgc.eastasia-01.azurewebsites.net/images/${req.file.filename}`
+        image_url:`https://ecommerce-fwczaqa5cwdrfsgc.eastasia-01.azurewebsites.net/images/${req.file.filename}`
     })
 }) 
 
