52ca8dbb747aa79374baf14e2fd2157bc5d52051
Saurabh9172 <9172169879misal@gmail.com>
2025-10-29 15:42:03 +0530

    updated

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,25 +1,35 @@
 import React, { useContext } from 'react';
-import {ShopContext} from '../Context/ShopContext';
+import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
-
 const Product = () => {
-  const {all_product} =useContext(ShopContext);
-  const {productId} = useParams();
-  const product= all_product.find((e)=>e.id===Number(productId));
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  // Wait until products are loaded
+  if (!all_product || all_product.length === 0) {
+    return <div style={{ textAlign: "center", marginTop: "50px" }}>Loading...</div>;
+  }
+
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  // If invalid productId or not found
+  if (!product) {
+    return <div style={{ textAlign: "center", marginTop: "50px" }}>Product not found</div>;
+  }
 
   return (
     <div>
       <Breadcrum product={product} />
-      <ProductDisplay product={product}/>
-      <DescriptionBox/>
-      <RelatedProducts category={product.category} currentProductId={product.id}/>
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts category={product.category} currentProductId={product.id} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
